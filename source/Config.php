e980ceec4281e14d1fdb49cc78bb1edc96de4db4
CaioFreitas96 <rlcmcaio@gmail.com>
2021-06-07 22:13:07 -0300

    atualização com composer

---
+++ source/Config.php
@@ -0,0 +1,12 @@
+<?php 
+
+session_start();
+
+define("FACEBOOK", [
+    "app_id" => "1899146013574319",
+    "app_secret" => "e62fd46581cf6839d1c5fae9c1fc6d58",
+    "app_redirect" => "http://localhost/login",
+    "app_version" => "v10.0"
+
+]);
+
